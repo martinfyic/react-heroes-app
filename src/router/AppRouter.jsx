2ca07cf6b804997ea475aad2c181ed510b9c051e
Martin Ferreira <martin.f.yic@gmail.com>
2023-05-23 22:20:15 -0300

    refactor: :art: better imports and add <Navbar />

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { MarvelPage, DcPage } from '../heroes/pages';
-import { LoginPage } from '../auth/pages';
+import { MarvelPage, DcPage } from '../heroes';
+import { LoginPage } from '../auth';
+import { Navbar } from '../ui';
 
 export const AppRouter = () => {
 	return (
 		<>
+			<Navbar />
 			<Routes>
 				<Route
 					path='/marvel'
