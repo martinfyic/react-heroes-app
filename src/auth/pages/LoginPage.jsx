4f9d920c2fab1270c52970b1286caac604e06d49
Martin Ferreira <martin.f.yic@gmail.com>
2023-05-26 21:48:12 -0300

    feat: :lipstick: create some ui component in LoginPage.jsx

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,3 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
 export const LoginPage = () => {
-	return <h1>LoginPage</h1>;
+	const navigation = useNavigate();
+
+	const onLogin = () => {
+		navigation('/', {
+			replace: true,
+		});
+	};
+
+	return (
+		<div className='container mt-5'>
+			<h1>Login</h1>
+			<hr />
+
+			<button
+				className='btn btn-primary'
+				onClick={onLogin}
+			>
+				Login
+			</button>
+		</div>
+	);
 };
