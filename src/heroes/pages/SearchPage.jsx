e398366f03e10599e06154725ff3cb97f7883ee2
Martin Ferreira <martin.f.yic@gmail.com>
2023-05-28 22:26:48 -0300

    refactor: :sparkles: create heroes/page/SearchPage.jsx

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,3 +1,79 @@
+import { useMemo } from 'react';
+import queryString from 'query-string';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useForm } from '../../hooks';
+import { getHerosByName } from '../helpers';
+import { HeroCard } from '../components';
+
 export const SearchPage = () => {
-	return <h1>SearchPage</h1>;
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	const { q = '' } = queryString.parse(location.search);
+	const heroesByName = useMemo(() => getHerosByName(q), [q]);
+
+	// Logica para mostrar errores en busqueda
+	const showSearch = q.length === 0;
+	const showError = q.length > 0 && heroesByName.length === 0;
+
+	const { searchText, onInputChange } = useForm({
+		searchText: q,
+	});
+
+	const onSearchSubmit = event => {
+		event.preventDefault();
+
+		navigate(`?q=${searchText.trim()}`);
+	};
+
+	return (
+		<>
+			<h1 className='fs-1 fw-bold text-center mt-3'>Search</h1>
+			<hr />
+
+			<div className='row mt-5'>
+				<div className='col-5'>
+					<h4 className='text-center'>Searching</h4>
+					<hr />
+					<form onSubmit={onSearchSubmit}>
+						<input
+							type='text'
+							placeholder='Search a hero'
+							className='form-control'
+							name='searchText'
+							autoComplete='off'
+							value={searchText}
+							onChange={onInputChange}
+						/>
+
+						<button className='btn btn-outline-primary mt-2'>Search</button>
+					</form>
+				</div>
+				<div className='col-7'>
+					<h4 className='text-center'>Results</h4>
+					<hr />
+					<div
+						className='alert alert-primary text-center mt-4 animate__animated animate__fadeIn'
+						style={{ display: showSearch ? '' : 'none' }}
+					>
+						Search a hero
+					</div>
+					<div
+						className='alert alert-danger text-center mt-4 animate__animated animate__shakeX'
+						style={{
+							display: showError ? '' : 'none',
+						}}
+					>
+						No results with <b>{q}</b>
+					</div>
+					{heroesByName.map(hero => (
+						<HeroCard
+							key={hero.id}
+							{...hero}
+						/>
+					))}
+				</div>
+			</div>
+		</>
+	);
 };
