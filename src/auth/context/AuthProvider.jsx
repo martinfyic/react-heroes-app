37acb58f157435a5f0c42b84d41ec9b601e30e9c
Martin Ferreira <martin.f.yic@gmail.com>
2023-05-31 20:42:01 -0300

    perf: :zap: start logic login user

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { useReducer } from 'react';
 import { AuthContext } from './AuthContext';
 import { authReducer } from './authReducer';
+import { types } from '../types/types';
 
 const initialState = {
 	logged: false,
@@ -9,5 +10,26 @@ const initialState = {
 export const AuthProvider = ({ children }) => {
 	const [authState, dispatch] = useReducer(authReducer, initialState);
 
-	return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+	const login = (name = '') => {
+		const action = {
+			type: types.login,
+			payload: {
+				id: 'A00012',
+				name: name,
+			},
+		};
+
+		dispatch(action);
+	};
+
+	return (
+		<AuthContext.Provider
+			value={{
+				...authState,
+				login: login,
+			}}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
 };
