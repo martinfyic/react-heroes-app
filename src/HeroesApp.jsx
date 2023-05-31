d4188ca4633193e53b59767a61a2bebdf0c1c419
Martin Ferreira <martin.f.yic@gmail.com>
2023-05-31 20:00:00 -0300

    perf: :building_construction: change import main to HeroesApp

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,5 +1,7 @@
 import { AuthProvider } from './auth';
 import { AppRouter } from './router/AppRouter';
+import 'animate.css';
+import './styles.css';
 
 export const HeroesApp = () => {
 	return (
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HeroesApp } from './HeroesApp.jsx';
-import 'animate.css';
-import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
