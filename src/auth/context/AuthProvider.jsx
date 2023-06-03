3e518690fb337f461b0b2bc9edcaeb089adc1e05
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-03 11:13:19 -0300

    refactor: :zap: user sesion localstorage

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,22 +3,34 @@ import { AuthContext } from './AuthContext';
 import { authReducer } from './authReducer';
 import { types } from '../types/types';
 
-const initialState = {
-	logged: false,
+const init = () => {
+	const user = JSON.parse(localStorage.getItem('user'));
+
+	return {
+		logger: !!user,
+		user: user,
+	};
 };
 
 export const AuthProvider = ({ children }) => {
-	const [authState, dispatch] = useReducer(authReducer, initialState);
+	const [authState, dispatch] = useReducer(authReducer, {}, init);
 
 	const login = (name = '') => {
+		const user = { id: 'ABC', name };
+
 		const action = {
 			type: types.login,
-			payload: {
-				id: 'A00012',
-				name: name,
-			},
+			payload: user,
 		};
 
+		localStorage.setItem('user', JSON.stringify(user));
+
+		dispatch(action);
+	};
+
+	const logout = () => {
+		localStorage.removeItem('user');
+		const action = { types: types.logout };
 		dispatch(action);
 	};
 
@@ -27,6 +39,7 @@ export const AuthProvider = ({ children }) => {
 			value={{
 				...authState,
 				login: login,
+				logout: logout,
 			}}
 		>
 			{children}
