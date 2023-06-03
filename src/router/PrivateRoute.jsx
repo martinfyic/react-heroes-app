2e2dd6c362c90e8d4cb4bc23c0c4498bb03c1206
Martin Ferreira <martin.f.yic@gmail.com>
2023-06-03 12:37:15 -0300

    perf: :zap: includ logic to last path when user logout

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,9 +1,15 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { AuthContext } from '../auth';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 export const PrivateRoute = ({ children }) => {
 	const { logged } = useContext(AuthContext);
+	const { pathname, search } = useLocation();
+
+	useEffect(() => {
+		const lastPath = pathname + search;
+		localStorage.setItem('lastPath', lastPath);
+	}, [pathname, search]);
 
 	return logged ? children : <Navigate to='/login' />;
 };
